e508f52fd8beeca531257c6da9f9c0bb6ec22ead
Ryan Chandler <ryangjchandler@gmail.com>
2022-01-27 18:04:06 +0000

    feature: transpile enums

--- bin/generate-php-parser.ts
+++ bin/generate-php-parser.ts
@@ -1,3 +1,4 @@
+import type { DeclarationStatement, ExportDeclaration, Expression, Statement, TSEnumMember } from "@typescript-eslint/types/dist/ast-spec";
 import { parse } from "@typescript-eslint/typescript-estree"
 import { readFileSync } from "fs";
 import path from "path";
@@ -10,7 +11,39 @@ FILES.forEach(file => {
     const contents = readFileSync(path.resolve(file), "utf-8")
     const tree = parse(contents)
 
+    let php = ''
     tree.body.forEach(node => {
-        console.log(node)
+        php = statementToPhp(node, php)
     })
+    console.log(php)
 })
+
+function expressionToPhp(expr: Expression, acc: string = ''): string {
+    if (expr.type === 'Identifier') {
+        acc += expr.name
+    }
+
+    return acc
+}
+
+function declarationToPhp(dclr: DeclarationStatement, acc: string = ''): string {
+    if (dclr.type === 'TSEnumDeclaration') {
+        acc += `enum ${dclr.id.name} {\n`
+
+        dclr.members.forEach((member: TSEnumMember) => {
+            acc += `    case ${expressionToPhp(member.id)};\n`
+        })
+
+        acc += `}`
+    }
+
+    return acc
+}
+
+function statementToPhp(stmt: Statement, acc: string = ''): string {
+    if (stmt.type === 'ExportNamedDeclaration') {
+        acc = declarationToPhp(stmt.declaration as DeclarationStatement, acc)
+    }
+
+    return acc
+}
